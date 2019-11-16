b40fa03401aa85f51a8d3143e2c22f1c72d8f09d
Unknown <jvictor2985@gmail.com>
2019-11-16 09:37:06 -0300

    Modificações no ArrayList de Denúncias

--- Urbano/app/src/main/java/br/edu/ifpe/tads/pdm/urbano/adapters/DenunciaArrayListAdapter.java
+++ Urbano/app/src/main/java/br/edu/ifpe/tads/pdm/urbano/adapters/DenunciaArrayListAdapter.java
@@ -1,10 +1,12 @@
 package br.edu.ifpe.tads.pdm.urbano.adapters;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,22 +16,32 @@ import br.edu.ifpe.tads.pdm.urbano.entidades.Denuncia;
 
 public class DenunciaArrayListAdapter extends ArrayAdapter<Denuncia> {
 
-    private ArrayList<Denuncia> denuncias;
+    Context context;
+    int resourceId;
+    LinearLayout linearLayout;
+    ArrayList<Denuncia> denuncias = new ArrayList<Denuncia>();
 
     public DenunciaArrayListAdapter(Context context, int resource, ArrayList<Denuncia> denuncias) {
         super(context, resource, denuncias);
+        this.resourceId = resource;
+        this.context = context;
         this.denuncias = denuncias;
     }
     @Override
     public View getView(int position, View view, ViewGroup parent) {
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-        View listItem = inflater.inflate(R.layout.denuncia_listitem, null, true);
+        View row = null;
+        if (view == null) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            row = inflater.inflate(resourceId, parent, false);
+        }   else {
+            row = view;
+        }
+    Denuncia denuncia = denuncias.get(position);
+        TextView tituloDenuncia = (TextView) row.findViewById(R.id.titulo_denuncia);
+        TextView descricaoDenuncia = (TextView) row.findViewById(R.id.info_local);
+        tituloDenuncia.setText(denuncia.getTitulo());
+        descricaoDenuncia.setText(denuncia.getDescricao());
 
-        TextView tituloDenuncia = (TextView) listItem.findViewById(R.id.titulo_denuncia);
-        TextView descricaoDenuncia = (TextView) listItem.findViewById(R.id.info_local);
-        tituloDenuncia.setText(denuncias.get(position).getTitulo());
-        descricaoDenuncia.setText(denuncias.get(position).getDescricao());
-
-        return listItem;
+        return row;
     }
 }

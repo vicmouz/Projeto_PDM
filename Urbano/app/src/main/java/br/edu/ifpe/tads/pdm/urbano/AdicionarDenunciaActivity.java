f63eccba5e81a3f98819fdc3ab467c8bdf592e29
Lidya Nascimento <lidyanascimento97@gmail.com>
2019-11-28 21:02:49 -0300

    adicionar denuncia modificacoes

--- Urbano/app/src/main/java/br/edu/ifpe/tads/pdm/urbano/AdicionarDenunciaActivity.java
+++ Urbano/app/src/main/java/br/edu/ifpe/tads/pdm/urbano/AdicionarDenunciaActivity.java
@@ -3,7 +3,9 @@ package br.edu.ifpe.tads.pdm.urbano;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -31,20 +33,23 @@ public class AdicionarDenunciaActivity extends AppCompatActivity {
 
     DatabaseReference drDenuncia;
     Button btn_adcionar_denuncia;
+    Button btn_go_Map;
+    Button btn_voltar;
 
     EditText titulo_denuncia;
     EditText descricao_denuncia;
     TextView latitude_denuncia;
     TextView longitude_denuncia;
 
-
     private static Usuario usuario = new Usuario();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_denuncia);
+
+        final SharedPreferences titulo_sharedPreferences = getSharedPreferences("titulo_denuncia", Context.MODE_PRIVATE);
+        final SharedPreferences descricao_sharedPreferences = getSharedPreferences("descricao_denuncia", Context.MODE_PRIVATE);
 
         //Pega a intent de outra activity
         Intent intent = getIntent();
@@ -61,10 +66,38 @@ public class AdicionarDenunciaActivity extends AppCompatActivity {
         latitude_denuncia.setText(lat.toString());
         longitude_denuncia.setText(lng.toString());
         btn_adcionar_denuncia = findViewById(R.id.btn_adc_nova_denuncia);
+        btn_go_Map = findViewById(R.id.btn_mapa);
+        btn_voltar = findViewById(R.id.btn_cancelar);
+
+        String tituloDenuncia = titulo_sharedPreferences.getString("titulo_denuncia", "");
+        String descricaoDenuncia = descricao_sharedPreferences.getString("descricao_denuncia", "");
+
+        if(tituloDenuncia != ""){
+            titulo_denuncia.setText(tituloDenuncia);
+        }
+
+        if(descricaoDenuncia != ""){
+            descricao_denuncia.setText(descricaoDenuncia);
+        }
+
+        btn_go_Map.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor titulo_editor = titulo_sharedPreferences.edit();
+                titulo_editor.putString("titulo_denuncia", titulo_denuncia.getText().toString());
+                titulo_editor.commit();
+
+                SharedPreferences.Editor descricao_editor = descricao_sharedPreferences.edit();
+                descricao_editor.putString("descricao_denuncia", descricao_denuncia.getText().toString());
+                descricao_editor.commit();
+
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        }));
+
 
         final FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-
 
         btn_adcionar_denuncia.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -96,6 +129,14 @@ public class AdicionarDenunciaActivity extends AppCompatActivity {
                         drDenuncia = fbDB_Denuncia.getReference("denuncias").push();
                         drDenuncia.setValue(denuncia);
 
+                        SharedPreferences.Editor titulo_editor = titulo_sharedPreferences.edit();
+                        titulo_editor.putString("titulo_denuncia", "");
+                        titulo_editor.commit();
+
+                        SharedPreferences.Editor descricao_editor = descricao_sharedPreferences.edit();
+                        descricao_editor.putString("descricao_denuncia", "");
+                        descricao_editor.commit();
+
                     }
                     @Override
                     public void onCancelled(DatabaseError databaseError) { }
@@ -113,13 +154,28 @@ public class AdicionarDenunciaActivity extends AppCompatActivity {
         }
 
         );
+
+        btn_voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor titulo_editor = titulo_sharedPreferences.edit();
+                titulo_editor.putString("titulo_denuncia", "");
+                titulo_editor.commit();
+
+                SharedPreferences.Editor descricao_editor = descricao_sharedPreferences.edit();
+                descricao_editor.putString("descricao_denuncia", "");
+                descricao_editor.commit();
+
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 
 
-    public void btnCancelar(View view){
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-    }
 
 
 }
